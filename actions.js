3ccf0d92d7081c1793f572ec90b127dc33b7c9a5
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2018-03-25 16:28:08 +0200

    Add comments & users reducer

--- actions.js
+++ actions.js
@@ -1,11 +1,11 @@
 import uuid from uuid;
 
 // type
-const ADD_COMMENT = 'ADD_COMMENT';
-const EDIT_COMMENT = 'EDIT_COMMENT';
-const REMOVE_COMMENT = 'REMOVE_COMMENT';
-const THUMB_UP = 'THUMB_UP';
-const THUMB_DOWN = 'THUMB_DOWN';
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+export const THUMB_UP = 'THUMB_UP';
+export const THUMB_DOWN = 'THUMB_DOWN';
 
 // action creators
 function addComment(text) {
