0cb4b318630dd364667c39408c74295e5ec7d465
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2018-03-27 21:13:32 +0200

    Add Logger

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import reducer from './reducer';
 import {addComment} from './actions';
 import './index.css';
+import {createLogger} from 'redux-logger';
 
-const store = createStore(reducer);
+const logger = createLogger();
+const store = createStore(reducer, applyMiddleware(logger));
 
 ReactDOM.render(
 	<Provider store={store}>
